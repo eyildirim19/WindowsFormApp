a812833d2a51a27cef31c7811f8426c7d155afb2
BTAkademimcsd <53770975+BTAkademimcsd@users.noreply.github.com>
2022-03-30 00:06:41 +0300

    hesap makinesi son

--- App6/Form1.Designer.cs
+++ App6/Form1.Designer.cs
@@ -31,20 +31,21 @@
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
             this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // flowLayoutPanel1
             // 
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 52);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 65);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
             this.flowLayoutPanel1.Size = new System.Drawing.Size(170, 151);
             this.flowLayoutPanel1.TabIndex = 0;
             // 
             // flowLayoutPanel2
             // 
-            this.flowLayoutPanel2.Location = new System.Drawing.Point(188, 52);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(187, 94);
             this.flowLayoutPanel2.Name = "flowLayoutPanel2";
-            this.flowLayoutPanel2.Size = new System.Drawing.Size(53, 151);
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(53, 117);
             this.flowLayoutPanel2.TabIndex = 1;
             // 
             // txtInput
@@ -59,12 +60,26 @@
             this.txtInput.Size = new System.Drawing.Size(228, 34);
             this.txtInput.TabIndex = 2;
             // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.IndianRed;
+            this.btnClear.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnClear.ForeColor = System.Drawing.Color.White;
+            this.btnClear.Location = new System.Drawing.Point(187, 64);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(54, 30);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "<-";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(253, 209);
+            this.ClientSize = new System.Drawing.Size(253, 228);
+            this.Controls.Add(this.btnClear);
             this.Controls.Add(this.txtInput);
             this.Controls.Add(this.flowLayoutPanel2);
             this.Controls.Add(this.flowLayoutPanel1);
@@ -85,5 +100,6 @@
         private FlowLayoutPanel flowLayoutPanel1;
         private FlowLayoutPanel flowLayoutPanel2;
         private TextBox txtInput;
+        private Button btnClear;
     }
 }
--- App6/Form1.cs
+++ App6/Form1.cs
@@ -1,3 +1,5 @@
+using System.Data;
+
 namespace App6
 {
     public partial class Form1 : Form
@@ -19,7 +21,12 @@ namespace App6
                 btn.Text = num;
                 btn.Width = 50;
                 btn.Height = 30;
-                btn.Click += Btn_Click;
+
+                if (num != "=")
+                    btn.Click += Btn_Click;
+                else
+                    btn.Click += Hesapla_Click;
+
                 flowLayoutPanel1.Controls.Add(btn);
             }
 
@@ -28,9 +35,79 @@ namespace App6
                 Button btn = new Button();
                 btn.Text = opt;
                 btn.Width = 50;
-                btn.Height = 30;
+                btn.Margin = new Padding(0, 1, 0, 0);
+                btn.Height = 27;
+                btn.Click += Btn_Click1;
+                btn.Enabled = false;
                 flowLayoutPanel2.Controls.Add(btn);
             }
+        }
+
+        string Hesapla()
+        {
+            string[] str = txtInput.Text.Split(islem);
+            if (txtInput.Text.IndexOf(islem) > 0)
+            {
+                double sayi1 = 0, sayi2 = 0, sonuc = 0;
+                sayi1 = Convert.ToDouble(str[0]);
+                sayi2 = Convert.ToDouble(str[1]);
+                switch (islem)
+                {
+                    case "+":
+                        sonuc = sayi1 + sayi2;
+                        break;
+                    case "*":
+                        sonuc = sayi1 * sayi2;
+                        break;
+                    case "/":
+                        sonuc = sayi1 / sayi2;
+                        break;
+                    case "-":
+                        sonuc = sayi1 - sayi2;
+                        break;
+                }
+                return sonuc.ToString();
+                //DataTable dt = new DataTable();
+                //object result = dt.Compute(txtInput.Text, "");
+                //return result.ToString();
+            }
+            else
+            {
+                return "a";
+            }
+        }
+
+        private void Hesapla_Click(object? sender, EventArgs e)
+        {
+            //DataTable dt = new DataTable();
+            //object result = dt.Compute(txtInput.Text, "");
+            //txtInput.Text = result.ToString();
+            txtInput.Text = Hesapla();
+        }
+
+        string islem = "";
+        private void Btn_Click1(object? sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            // DisableOperatorsButton(false);
+
+            // son karakter
+            string str = txtInput.Text[txtInput.Text.Length - 1].ToString();
+            if (operatos.IndexOf(str) > -1) // operatör karakteri ise
+            {
+                txtInput.Text = txtInput.Text.Replace(str, "");
+            }
+
+            string t = Hesapla();
+            if (t != "a")
+            {
+                txtInput.Text = t + btn.Text;
+            }
+            else
+            {
+                txtInput.Text += btn.Text;
+            }
+            islem = btn.Text;
         }
 
         // e parametresi => eventi temsil eder..
@@ -39,16 +116,45 @@ namespace App6
         private void Btn_Click(object? sender, EventArgs e)
         {
             Button btn = (Button)sender;
-            //if (btn.Text == ",")
-            //{
-            //    if (txtInput.Text.IndexOf(",") >= 0)
-            //    {
-            //        return;
-            //    }
-            //}
             txtInput.Text += btn.Text;
             virgulEngelle();
+            DisableOperatorsButton(true);
         }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Show metodu dönüþ tipi DialogResulttýr. Dialog buttonlardan hangisine týklanrýrsa o tipte result döner..
+            DialogResult result = MessageBox.Show("Uygulama Kapanacak. \nEmin misin?", "Uyarý", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+
+            if (result == DialogResult.Cancel) // result cancel ise
+            {
+                e.Cancel = true; // formu kapatma....
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+
+            // tek tek temizle
+            if (txtInput.Text.Length > 0)
+                txtInput.Text = txtInput.Text.Remove(txtInput.Text.Length - 1, 1);
+
+
+
+            // eðer text silindiyse
+            if (txtInput.Text.Length == 0)
+                DisableOperatorsButton(false); // iþlem operatörleri disable et...
+        }
+
+        void DisableOperatorsButton(bool isEnable)
+        {
+            // Control
+            foreach (Control control in flowLayoutPanel2.Controls)
+            {
+                control.Enabled = isEnable;
+            }
+        }
+
         void virgulEngelle()
         {
             int index = txtInput.Text.IndexOf(",");
@@ -58,21 +164,11 @@ namespace App6
             }
             else if (index > 0)
             {
-                index = txtInput.Text.IndexOf(",", index+1);
+                index = txtInput.Text.IndexOf(",", index + 1);
                 if (index > 0)
                 {
                     txtInput.Text = txtInput.Text.Remove(index, 1);
                 }
-            }
-        }
-        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
-        {
-            // Show metodu dönüþ tipi DialogResulttýr. Dialog buttonlardan hangisine týklanrýrsa o tipte result döner..
-            DialogResult result = MessageBox.Show("Uygulama Kapanacak. \nEmin misin?", "Uyarý", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
-
-            if (result == DialogResult.Cancel) // result cancel ise
-            {
-                e.Cancel = true; // formu kapatma....
             }
         }
     }
